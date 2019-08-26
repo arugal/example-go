5f12bc0f6b5ba9364f34a4914f0c6f874c232132
zhangwei <pknfe@outlook.com>
2019-08-26 23:51:11 +0800

    add net example

--- net_test.go
+++ net_test.go
@@ -12,3 +12,49 @@ func TestResolveIPAddr(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestDialTCP(t *testing.T) {
+	port := "8282"
+	listen, err := listen(port)
+	if err != nil {
+		t.Error(err)
+	}
+	defer listen.Close()
+	conn, err := net.Dial("tcp", "127.0.0.1:"+port)
+	if err != nil {
+		t.Error(err)
+	}
+	defer conn.Close()
+	read := []byte{1}
+	s, err := conn.Read(read)
+	if err != nil {
+		t.Error(err)
+	}
+	if s != 1 {
+		t.Failed()
+	}
+	if read[0] != 0 {
+		t.Failed()
+	}
+}
+
+func listen(port string) (net.Listener, error) {
+	listen, err := net.Listen("tcp", "127.0.0.1:"+
+		port)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			conn, err := listen.Accept()
+			if err != nil {
+				continue
+			}
+			defer conn.Close()
+			_, _ = conn.Write([]byte{0})
+		}
+	}()
+
+	return listen, nil
+}
