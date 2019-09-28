ee7ea5a384d11ca243e31c3fab8595489d6242f2
zhangwei <pknfe@outlook.com>
2019-09-28 22:59:59 +0800

    add curl

--- mysql/tool.sh
+++ mysql/tool.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-curl http://127.0.0.1:9000/pool
+# insert
+curl -X POST http://127.0.0.1:9000/dao/insert  -H "Content-Type:application/json" -d '{"name":"curl", "age":30}' -v
+
+# update
+curl http://127.0.0.1:9000/dao/update -X POST -H "Content-Type:application/json" -d '{"id":1, "name":"curl", "age":30}' -v
+
+# select
+curl http://127.0.0.1:9000/dao/select -v
