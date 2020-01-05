f7e8dc36eefa03e11167b4f3211b1dc4dd0e1331
zhangwei <pknfe@outlook.com>
2020-01-05 14:55:35 +0800

    add istio demo script

---
+++ istio-demo/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_DIR="$(cd "$(dirname $0)"; pwd)"
+EXAMPLE_HOME=${CURRENT_DIR}../
+
+kubectl delete -f ${CURRENT_DIR}/istio-demo.yaml
+kubectl delete -f ${CURRENT_DIR}/istio-demo-gateway.yaml
+
+
+docker rmi project-a:1.0
+docker rmi project-b:1.0
+
+docker system prune
