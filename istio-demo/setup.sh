06e3657d631cfa637e88bb9f1b678f43df8e948f
zhangwei <pknfe@outlook.com>
2020-01-05 14:58:26 +0800

    fix script bug

--- istio-demo/setup.sh
+++ istio-demo/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_DIR="$(cd "$(dirname $0)"; pwd)"
-EXAMPLE_HOME=${CURRENT_DIR}../
+EXAMPLE_HOME=${CURRENT_DIR}/../
 
 docker build -f ${CURRENT_DIR}/projectA/Dockerfile -t project-a:1.0 ${EXAMPLE_HOME}
 docker build -f ${CURRENT_DIR}/projectB/Dockerfile -t project-b:1.0 ${EXAMPLE_HOME}
