eef351b05c89b8ec05545db37d858e67002147a8
zhangwei <pknfe@outlook.com>
2020-01-05 14:57:12 +0800

    fix script bug

--- istio-demo/cleanup.sh
+++ istio-demo/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_DIR="$(cd "$(dirname $0)"; pwd)"
-EXAMPLE_HOME=${CURRENT_DIR}../
+EXAMPLE_HOME=${CURRENT_DIR}/../
 
 kubectl delete -f ${CURRENT_DIR}/istio-demo.yaml
 kubectl delete -f ${CURRENT_DIR}/istio-demo-gateway.yaml
